51f5ee30327189e38700e08ca7de14c08a43f57c
Yunkyu Choi <ckyun777@gmail.com>
2016-11-22 04:06:57 +0900

    Add mesh inspector properties, Delete unnecessary fonts

--- Assets/5_YKExamples2016/2_Scritps/RawImageExtended.cs
+++ Assets/5_YKExamples2016/2_Scritps/RawImageExtended.cs
@@ -6,32 +6,38 @@ using UnityEngine.EventSystems;
 
 public class RawImageExtended : RawImage {
 
-	public Vector3 m_TestVec3 = Vector3.one;
-	
-	public Vector3[] m_Verts = { 	new Vector3 ( 	0f, 	50f), 
-                         			new Vector3 ( -100f, -50f),
-									new Vector3 (  100f, -50f)
-								};
+	[SerializeField] public Vector3 m_TestVec3 = Vector3.one;
+	[SerializeField] public AnimationCurve m_TestCurve;
+	[SerializeField] public Vector3[] m_Verts = { 	new Vector3 ( 	0f, 	50f), 
+                         			new Vector3 ( 100f, -50f),
+											new Vector3 ( 	-100f, -50f)
+										};
+
+	protected RawImageExtended()
+	{
+		//useLegacyMeshGeneration = false;
+		m_TestCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+	}
 
 	protected override void OnPopulateMesh(VertexHelper vh)
-        {
-            Texture tex = mainTexture;
-            vh.Clear();
-            if (tex != null)
-            {
-                var r = GetPixelAdjustedRect();
-                var v = new Vector4(r.x, r.y, r.x + r.width, r.y + r.height);
-                var scaleX = tex.width * tex.texelSize.x;
-                var scaleY = tex.height * tex.texelSize.y;
-                {
-                    var color32 = color;
-                    vh.AddVert(m_Verts[0], color32, new Vector2(0.5f, 1f));
-                    vh.AddVert(m_Verts[1], color32, new Vector2(0f, 0f));
-                    vh.AddVert(m_Verts[2], color32, new Vector2(1f, 0f));
-
-                    vh.AddTriangle(0, 1, 2);
-                    //vh.AddTriangle(2, 3, 0);
-                }
-            }
-        }
+	{
+	   Texture tex = mainTexture;
+	   vh.Clear();
+	   if (tex != null)
+	   {
+	       var r = GetPixelAdjustedRect();
+	       var v = new Vector4(r.x, r.y, r.x + r.width, r.y + r.height);
+	       var scaleX = tex.width * tex.texelSize.x;
+	       var scaleY = tex.height * tex.texelSize.y;
+	       {
+	           var color32 = color;
+	           vh.AddVert(m_Verts[0], color32, new Vector2(0.5f, 1f));
+	           vh.AddVert(m_Verts[1], color32, new Vector2(0f, 0f));
+	           vh.AddVert(m_Verts[2], color32, new Vector2(1f, 0f));
+	
+	           vh.AddTriangle(0, 1, 2);
+	           //vh.AddTriangle(2, 3, 0);
+	       }
+	   }
+}
 }
