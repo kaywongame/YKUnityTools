a537e93dbef2c04098315e43ea71139ffb0d7fe1
Yunkyu Choi <ckyun777@gmail.com>
2016-11-22 16:45:23 +0900

    UI follows a 3D world object

--- Assets/5_YKExamples2016/2_Scritps/UiFollowWorld.cs
+++ Assets/5_YKExamples2016/2_Scritps/UiFollowWorld.cs
@@ -6,16 +6,23 @@ public class UiFollowWorld : MonoBehaviour {
 	public Camera m_WorldCamera;
 	public Transform m_WorldObjTransform;
 	public RectTransform m_UiRectTransform;
+	private float m_ScreenRatio = 1f;
 
 	// Use this for initialization
 	void Start () {
-
+		m_ScreenRatio = Screen.width / Screen.height;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		m_ScreenRatio = Screen.width / Screen.height;
 
 		Vector2 viewportPos = m_WorldCamera.WorldToViewportPoint(m_WorldObjTransform.position);
+
+		viewportPos = new Vector2(
+			m_ScreenRatio * viewportPos.x - ((m_ScreenRatio - 1) * 0.5f), 
+			viewportPos.y); 
+		
 		m_UiRectTransform.anchorMin = viewportPos;
         m_UiRectTransform.anchorMax = viewportPos;
         //m_UiRectTransform.sizeDelta = new Vector2(500f, 200f);
---
+++ Assets/5_YKExamples2016/2_Scritps/UiFollowWorld3D.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiFollowWorld3D : MonoBehaviour {
+	
+	public Transform m_WorldObjTransform;
+	public RectTransform m_UiRectTransform;
+	
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		m_UiRectTransform.position = m_WorldObjTransform.position;
+		
+	}
+}
