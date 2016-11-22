cf83ffa2aa6ad5e1e90b6855947dd15f50633e3b
kaywongame <kaywon.game@gmail.com>
2016-11-22 12:06:02 +0900

    trivial

--- Assets/5_YKExamples2016/2_Scritps/UiFollowWorld.cs
+++ Assets/5_YKExamples2016/2_Scritps/UiFollowWorld.cs
@@ -7,15 +7,24 @@ public class UiFollowWorld : MonoBehaviour {
 	public Transform m_WorldObjTransform;
 	public RectTransform m_UiRectTransform;
 
+    public RectTransform m_UiCanvasRect;
+    public float m_WidthAdj = 1f;
+
 	// Use this for initialization
 	void Start () {
-
+        //Debug.Log(Screen.width);
+        //m_UiCanvasRect.sizeDelta.Set(Screen.width * 0.1f, Screen.height * 0.1f);
+        m_WidthAdj = Screen.width / Screen.height;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //Debug.Log(Screen.width);
+        Vector2 viewportPos = m_WorldCamera.WorldToViewportPoint(m_WorldObjTransform.position);
 
-		Vector2 viewportPos = m_WorldCamera.WorldToViewportPoint(m_WorldObjTransform.position);
+        viewportPos = new Vector2(
+            ((viewportPos.x - 0.5f) * m_WidthAdj) + 0.5f, 
+            viewportPos.y);
 		m_UiRectTransform.anchorMin = viewportPos;
         m_UiRectTransform.anchorMax = viewportPos;
         //m_UiRectTransform.sizeDelta = new Vector2(500f, 200f);
