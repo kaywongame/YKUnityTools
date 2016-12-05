4cc6fef527ea0a5cb7123843ac98fc29cafb3cb5
Yunkyu Choi <ckyun777@gmail.com>
2016-12-05 12:26:15 +0900

    trivial

--- Assets/7_UnityTools/Scritps/GameSystem/GORecycleManager.cs
+++ Assets/7_UnityTools/Scritps/GameSystem/GORecycleManager.cs
@@ -2,76 +2,83 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GORecycleManager : MonoBehaviour {
-	
-	public Queue<GameObject> m_ReadyGO;
+public class GORecycleManager : MonoBehaviour
+{
 
-	public List<GameObject> m_InUseGO;
-	
-	public GameObject m_SourceGO;
-	public int m_NoOfCopy = 10;
-	
-	// Use this for initialization
-	void Start () {
-		m_ReadyGO = new Queue<GameObject>();
-		
-		for (int i = 0; i < m_NoOfCopy; i++) {
-			GameObject go = (GameObject)Instantiate(m_SourceGO, new Vector3(0f, -100f, 0f), Quaternion.identity);
-			go.SetActive(false);
-			m_ReadyGO.Enqueue( go );
-		}
-		/*
-		foreach (Transform item in transform) {
-			m_ReadyGO.Enqueue( item.gameObject );
-			item.parent = null;
-		}
-		
-		foreach (GameObject item in m_ReadyGO) {
-			print("in ready queue" + item.name);
-		}
-		*/
-	}
-	
-	public GameObject GetGO()
-	{
-		if(m_ReadyGO.Count < 1) 
-		{
-			Debug.LogError("No GO in Queue");
-			return null;
-		}
-		
-		GameObject go = m_ReadyGO.Dequeue();
-		
-		m_InUseGO.Add(go);
-		
-		// Detatch From parent
-		//go.transform.parent = null;
-		go.SetActive(true);
-		
-		return go;
-	}
-	
-	public void DontUseGO( GameObject a_GO )
-	{
-		if(!m_ReadyGO.Contains(a_GO))
-		{
-			m_ReadyGO.Enqueue( a_GO );
-		m_InUseGO.Remove( a_GO );
+    public Queue<GameObject> m_ReadyGO;
 
-        //a_GO.transform.parent = gameObject.transform;
-        a_GO.SetActive(false);
-		}
-	}
+    public List<GameObject> m_InUseGO;
+
+    public GameObject m_SourceGO;
+    public int m_NumOfCopy = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_ReadyGO = new Queue<GameObject>();
+
+        for (int i = 0; i < m_NumOfCopy; i++)
+        {
+            GameObject go = (GameObject)Instantiate(m_SourceGO, new Vector3(0f, -100f, 0f), Quaternion.identity);
+            go.SetActive(false);
+            m_ReadyGO.Enqueue(go);
+        }
+        /*
+        foreach (Transform item in transform) {
+            m_ReadyGO.Enqueue( item.gameObject );
+            item.parent = null;
+        }
+		
+        foreach (GameObject item in m_ReadyGO) {
+            print("in ready queue" + item.name);
+        }
+        */
+    }
+
+    public GameObject GetGO()
+    {
+        if (m_ReadyGO.Count < 1)
+        {
+            Debug.LogError("No GO in Queue");
+            return null;
+        }
+
+        GameObject go = m_ReadyGO.Dequeue();
+
+        m_InUseGO.Add(go);
+
+        // Detatch From parent
+        //go.transform.parent = null;
+        go.SetActive(true);
+
+        return go;
+    }
+
+    public void DontUseGO(GameObject a_GO)
+    {
+        if (!m_ReadyGO.Contains(a_GO))
+        {
+            m_ReadyGO.Enqueue(a_GO);
+            m_InUseGO.Remove(a_GO);
+
+            //a_GO.transform.parent = gameObject.transform;
+            a_GO.SetActive(false);
+        }
+    }
 
     public void DontUseAllGOs()
     {
         GameObject[] GOsInUse = new GameObject[m_InUseGO.Count];
         m_InUseGO.CopyTo(GOsInUse);
 
-        foreach (GameObject item in GOsInUse)
+        for (int i = 0; i < GOsInUse.Length; i++)
         {
-            DontUseGO(item);
+            DontUseGO(GOsInUse[i]);
         }
+        //foreach (GameObject item in GOsInUse)
+        //{
+        //	DontUseGO(item);
+        //}
     }
 }
 
