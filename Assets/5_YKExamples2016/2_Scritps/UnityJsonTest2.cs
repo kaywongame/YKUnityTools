7d5b903236101ce79223c6fb7b060d98f5b7b528
Yunkyu Choi <ckyun777@gmail.com>
2016-12-17 05:05:48 +0900

    Json ex

---
+++ Assets/5_YKExamples2016/2_Scritps/UnityJsonTest2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class UnityJsonTest2 : MonoBehaviour {
+
+	[Serializable]
+	public class MyClass
+	{
+		public int level;
+		public float timeElapsed;
+		public string playerName;
+		
+	}
+	
+	[Serializable]
+	public class MyClassArray
+	{
+		public MyClass[] classArray = new MyClass[2];
+	}
+
+	//public MyClass[] m_DataArray = new MyClass[3];
+	public MyClassArray m_DataArrayClass;
+
+	public string m_JsonString;
+
+	//public MyClass[] m_LoadedData;
+	public MyClassArray m_LoadedDataArray;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.S))
+		{
+			m_JsonString = JsonUtility.ToJson(m_DataArrayClass);
+		}
+
+		if(Input.GetKeyUp(KeyCode.L))
+		{
+			m_LoadedDataArray = JsonUtility.FromJson<MyClassArray>(m_JsonString);	
+		}
+
+	}
+}
---
+++ Assets/Net20/JsonDict.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using UnityEngine.UI;
+
+public class JsonDict : MonoBehaviour {
+
+	
+	[Serializable]
+	public class MyStruct{
+
+		public Dictionary<string, int> points;
+	}
+	
+	public MyStruct m_Struct;	 
+	public string json;
+	public MyStruct m_StructLoaded;	 
+
+	public Text Output;
+
+    void WriteLine(string msg)
+    {
+        Output.text = Output.text + msg + "\n";
+    }
+
+	// Use this for initialization
+	void Start () {
+		m_Struct.points = new Dictionary<string, int>
+		 {
+		     { "James", 9001 },
+		     { "Jo", 3474 },
+		     { "Jess", 11926 }
+		 };
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.S))
+		{
+			json = JsonConvert.SerializeObject(m_Struct, Formatting.Indented);
+			WriteLine(json);
+		}
+
+		if(Input.GetKeyUp(KeyCode.L))
+		{
+			m_StructLoaded = (MyStruct)JsonConvert.DeserializeObject(json, typeof(MyStruct));
+
+			foreach (var item in m_StructLoaded.points) {
+				Debug.Log(item.Key + " : " + item.Value);
+			}
+		}
+
+		
+	}
+}
